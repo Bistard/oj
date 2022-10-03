aa413e79eaadced46ef9169fb38f4a8501475004
SIHAN LI <1015813038@qq.com>
2022-10-02 22:16:52 -0400

    [Add] lc 35 189 278 977

---
+++ leetcode/278.first-bad-version.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=278 lang=cpp
+ *
+ * [278] First Bad Version
+ */
+
+// @lc code=start
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        size_t l = -1, r = n;
+        size_t m;
+        bool isBad;
+
+        while (l + 1 != r) {
+
+            m = (l + r) / 2;
+
+            isBad = isBadVersion(m);
+            if (!isBad && isBadVersion(m + 1)) {
+                return m + 1;
+            }
+
+            if (isBad) {
+                r = m;
+            } else {
+                l = m;
+            }
+        }
+
+        return m;
+    }
+};
+// @lc code=end
+
