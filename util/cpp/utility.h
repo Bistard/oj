83bc86b31327368262ede6a2b739a24fb26f256f
Bistard <lshh1015813038@gmail.com>
2022-10-05 20:50:12 -0400

    [Modify] remove UnionFind from utility.h

--- util/cpp/utility.h
+++ util/cpp/utility.h
@@ -130,36 +130,4 @@ ostream &operator<<(ostream &out, const unordered_map<K, V> &map) {
 }
 #endif
 
-#ifndef __data_structure__
-#define __data_structure__
-
-class UnionFind {
-
-public:
-    UnionFind(int n): _parents { vi(n) } {
-        REPEAT(i, n) {
-            _parents[i] = i;
-        }
-    }
-
-    int find(int x) {
-        if (_parents[x] != x) {
-            _parents[x] = find(_parents[x]);
-        }
-        return _parents[x];
-    }
-
-    void unions(int x, int y) {
-        _rootx = find(x);
-        _rooty = find(y);
-        _parents[_rootx] = _rooty;
-    }
-
-public:
-    vi _parents;
-    int _rootx, _rooty;
-};
-
-#endif
-
 #endif
