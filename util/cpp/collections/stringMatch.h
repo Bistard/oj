a05a9a3a5a0ad91b0b6b7911b16b258bc3cce418
Bistard <lshh1015813038@gmail.com>
2022-10-08 03:31:23 -0400

    [New] cpp stringMatch.h && unionFind.h

---
+++ util/cpp/collections/stringMatch.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "./marcos.h"
+
+class StringMatchByKmp {
+public:
+    int match(const string &s, const string &pattern) {
+        const int n = s.length();
+        const int m = pattern.length();
+        
+        // lps
+        int state = 0;
+        int i = 1;
+        vi lps(m, 0);
+        while (i < m) {
+            if (pattern[i] == pattern[state]) {
+                lps[i] = state + 1;
+                state++;
+                i++;
+            }
+            else {
+                if (state == 0) {
+                    lps[i] = 0;
+                    i++;
+                } else {
+                    state = lps[state - 1];
+                }
+            }
+        }
+        
+        // kmp
+        i = 0;
+        state = 0;
+        while (i < n) {
+            if (s[i] == pattern[state]) {
+                i++;
+                state++;
+            } else {
+                if (state == 0) {
+                    i++;
+                } else {
+                    state = lps[state - 1];
+                }
+            }
+
+            if (state == m) {
+                return i - m;
+            }
+        }
+        
+        return -1;
+    }
+};
