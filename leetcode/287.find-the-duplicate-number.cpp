67ceae72501f9965c7d8156f9f03eb4e199e9fa4
Bistard <lshh1015813038@gmail.com>
2022-10-15 22:30:48 -0400

    [Add] lc 287 268

---
+++ leetcode/287.find-the-duplicate-number.cpp
@@ -0,0 +1,290 @@
+/*
+ * @lc app=leetcode id=287 lang=cpp
+ *
+ * [287] Find the Duplicate Number
+ */
+
+// @lc code=start
+
+#ifndef __utility__
+#define __utility__
+
+#include <bits/stdc++.h> // Include every standard library(s)
+using namespace std;
+
+#ifndef __constants__
+#define __constants__
+
+const double PI = acos(-1);
+const int directions[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+const int directions8[8][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}};
+#endif
+
+#ifndef __marcos__
+#define __marcos__
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+typedef pair<int, int> pii;
+typedef pair<string, string> pss;
+typedef pair<int, string> pis;
+typedef pair<string, int> psi;
+
+typedef set<int> si;
+
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<vi> vvi;
+typedef vector<vc> vvc;
+typedef vector<vb> vvb;
+typedef vector<vs> vvs;
+
+typedef unordered_map<int, int> mii;
+typedef unordered_map<char, char> mcc;
+typedef unordered_map<int, string> mis;
+typedef unordered_map<string, int> msi;
+typedef unordered_map<string, string> mss;
+#define umap unordered_map
+
+// bit && number
+#define BIT(x, i) (x & (1 << i)) // select the bit at the position i of x. eg: BIT(5, 0) => 1; BIT(5, 2) => 4;
+#define LOWBIT(x) ((x) & ((x) ^ ((x) - 1))) // get the lowest bit of x
+#define SETMAX(a, b) (a) = max((a), (b))
+#define SETMIN(a, b) (a) = min((a), (b))
+#define CLAMP(n, min, max) ((n <= min) ? min : (n >= max ? max : n))
+#define SQ(n) ((n) * (n))
+#define INTMAX __INT_MAX__
+#define INTMIN (-INTMAX - 1)
+#define SUM(arr) (reduce(arr.begin(), arr.end()))
+
+// array
+#define arr2d(v, n, m, fill) vvi v((n), vi((m), (fill)))
+#define arr2dc(v, n, m, fill) vvc v((n), vc((m), (fill)))
+#define arr2db(v, n, m, fill) vvb v((n), vb((m), (fill)))
+#define charmap(map, size, fill) char map[size] = { fill };
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+
+// boundary
+#define IN(i, l, r) (((l) < (i)) && ((i) < (r)))
+#define INL(i, l, r) (((l) <= (i)) && ((i) < (r)))
+#define INR(i, l, r) (((l) < (i)) && ((i) <= (r)))
+#define INLR(i, l, r) (((l) <= (i)) && ((i) <= (r)))
+
+// iterator
+#define ALL(c) (c).begin(), (c).end()
+#define SORT(c) sort(ALL(c))
+#define REVERSE(c) reverse(ALL(c))
+#define HAS(c, v) ((c).find(v) != (c).end())
+#define WHILEIT(it, c) auto it = (c).begin(); while (it != (c).end())
+
+// loop
+#define FOR(i, l, r) for (int (i) = (l); (i) < (r); ++(i))
+#define FORE(i, l, r) for (int (i) = (l); (i) <= (r); ++(i))
+#define FFOR(i, l, r) for (int (i) = (l); (i) > (r); --(i))
+#define FFORE(i, l, r) for (int (i) = (l); (i) >= (r); --(i))
+#define FOREACH(v, c) for (auto &(v) : (c))
+#define REPEAT(i, n) FOR(i, 0, n)
+#define REPEATN(i, n) FORE(i, 1, n)
+#endif
+
+#ifndef __pretty_print__
+#define __pretty_print__
+
+#define IOS ios_base::sync_with_stdio(0) // to synchronize the input of cin and scanf
+#define BREAK cout << "===========" << endl
+#define VALUE(x) cout << "The value of \"" << #x << "\" is: " << x << endl
+
+template<typename K, typename V>
+ostream &operator<<(ostream &out, const pair<K, V> &pair)
+{
+    out << "<" << pair.first << ", " << pair.second << ">";
+    return out;
+}
+
+template<typename T>
+static inline ostream &__print_iterable(ostream &out, const T &container) 
+{
+    auto it = container.begin();
+    out << "{ " << *it++;
+    while (it != container.end()) {
+        out << ", " << *it++;
+    }
+    out << " }";
+    return out;
+}
+
+template<typename T>
+ostream &operator<<(ostream &out, const vector<T> &vec) {
+    return __print_iterable(out, vec);
+}
+
+template<typename T, size_t Size>
+ostream &operator<<(ostream &out, const array<T, Size> &arr) {
+    return __print_iterable(out, arr);
+}
+
+template<typename T>
+ostream &operator<<(ostream &out, const set<T> &set) {
+    return __print_iterable(out, set);
+}
+
+template<typename T>
+ostream &operator<<(ostream &out, const stack<T> &s) {
+    out << "[";
+    
+    if (!s.empty()) {
+        stack<T> copy = s;
+        out << copy.top();
+        copy.pop();
+
+        while (!copy.empty()) {
+            out << ", ";
+            auto top = copy.top();
+            out << top;
+            copy.pop();
+        }
+    }
+
+    out << "]";
+    return out;
+}
+
+template<typename T>
+ostream &operator<<(ostream &out, const queue<T> &q) {
+    out << "[";
+    
+    if (!q.empty()) {
+        queue<T> copy = q;
+        out << copy.front();
+        copy.pop();
+
+        while (!copy.empty()) {
+            out << ", ";
+            auto front = copy.front();
+            out << front;
+            copy.pop();
+        }
+    }
+
+    out << "]";
+    return out;
+}
+
+template<typename T>
+ostream &operator<<(ostream &out, const priority_queue<T> &pq) {
+    return __print_iterable(out, pq); // BUG
+}
+
+template<typename T>
+ostream &operator<<(ostream &out, const deque<T> &deq) {
+    return __print_iterable(out, deq); // BUG
+}
+
+template<typename K, typename V>
+ostream &operator<<(ostream &out, const map<K, V> &map) {
+    return __print_iterable(out, map);
+}
+
+template<typename K, typename V>
+ostream &operator<<(ostream &out, const unordered_map<K, V> &map) {
+    return __print_iterable(out, map);
+}
+#endif
+
+#endif
+
+// tag: fast-slow pointers; time: O(n); space: O(1)
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        const int n = nums.size();
+        
+        int p1 = nums[0], p2 = nums[0];
+        do {
+            p1 = nums[p1];       // slow ptr goes 1 step
+            p2 = nums[nums[p2]]; // fast ptr goes 2 steps
+        } while (p1 != p2);
+
+        p1 = nums[0];
+        while (p1 != p2) {
+            p1 = nums[p1];
+            p2 = nums[p2];
+        }
+        
+        return p1;
+    }
+};
+
+// tag: set; time: O(n); space: O(n)
+class Set {
+public:
+    int findDuplicate(vector<int>& nums) {
+        const int n = nums.size();
+        
+        set<int> s;
+
+        FOREACH(num, nums) {
+            if (HAS(s, num)) {
+                return num;
+            }
+            s.insert(num);
+        }
+        
+        return -1;
+    }
+};
+
+// tag: binary search; time: O(nlogn + logn); sapce: O(1)
+class BinarySearch {
+public:
+    int findDuplicate(vector<int>& nums) {
+        const int n = nums.size();
+        
+        SORT(nums);
+
+        int l = -1, r = n;
+        int m;
+        while (l + 1 != r) {
+            m = l + (r - l) / 2;
+            if (m > 0 && nums[m] == nums[m - 1]) {
+                return nums[m];
+            } else if (m < n - 1 && nums[m] == nums[m + 1]) {
+                return nums[m];
+            }
+
+            if (nums[m] <= m) {
+                r = m;
+            } else {
+                l = m;
+            }
+        }
+        
+        return -1;
+    }
+};
+
+// tag: sorting: time: O(nlogn + n); sapce: O(1)
+class Sorting {
+public:
+    int findDuplicate(vector<int>& nums) {
+        const int n = nums.size();
+        SORT(nums);
+
+        FOR(i, 1, n) {
+            if (nums[i] == nums[i - 1]) {
+                return nums[i];
+            }
+        }
+        
+        return -1;
+    }
+};
+// @lc code=end
+
