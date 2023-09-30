5340653ec2d5e74cde43fa0cc997a83736239403
SIHAN LI <lshh1015813038@gmail.com>
2023-09-29 23:10:48 -0400

    [Add] lc 643/1456/1679

---
+++ leetcode/code/1456.maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -0,0 +1,260 @@
+/*
+ * @lc app=leetcode id=1456 lang=cpp
+ *
+ * [1456] Maximum Number of Vowels in a Substring of Given Length
+ */
+
+// @lc code=start
+
+#ifndef __utility__
+#define __utility__
+
+#include <bits/stdc++.h> // Include every standard library(s)
+using namespace std;
+
+#ifndef __constants__
+#define __constants__
+
+const double PI = acos(-1);
+const int directions[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+const int directions8[8][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}};
+#endif
+
+#ifndef __marcos__
+#define __marcos__
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+typedef pair<int, int> pii;
+typedef pair<string, string> pss;
+typedef pair<int, string> pis;
+typedef pair<string, int> psi;
+
+typedef unordered_set<int> si;
+#define uset unordered_set
+
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<vi> vvi;
+typedef vector<vc> vvc;
+typedef vector<vb> vvb;
+typedef vector<vs> vvs;
+#define vec vector
+
+typedef unordered_map<int, int> mii;
+typedef unordered_map<char, char> mcc;
+typedef unordered_map<int, string> mis;
+typedef unordered_map<string, int> msi;
+typedef unordered_map<string, string> mss;
+#define umap unordered_map
+
+// bit && number
+#define MODULO (1e9 + 7)
+#define BIT(x, i) (x & (1 << i)) // select the bit at the position i of x. eg: BIT(5, 0) => 1; BIT(5, 2) => 4;
+#define LOWBIT(x) ((x) & ((x) ^ ((x) - 1))) // get the lowest bit of x
+#define SETMAX(a, b) (a) = max((a), (b))
+#define SETMIN(a, b) (a) = min((a), (b))
+#define CLAMP(n, min, max) ((n <= min) ? min : (n >= max ? max : n))
+#define SQ(n) ((n) * (n))
+#define INTMAX __INT_MAX__
+#define INTMIN (-INTMAX - 1)
+
+// array
+#define arr2d(v, n, m, fill) vvi v((n), vi((m), (fill)))
+#define arr2dc(v, n, m, fill) vvc v((n), vc((m), (fill)))
+#define arr2db(v, n, m, fill) vvb v((n), vb((m), (fill)))
+#define charmap(map, size, fill) char map[size] = { fill }
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+
+// boundary
+#define IN(i, l, r) (((l) < (i)) && ((i) < (r)))
+#define INL(i, l, r) (((l) <= (i)) && ((i) < (r)))
+#define INR(i, l, r) (((l) < (i)) && ((i) <= (r)))
+#define INLR(i, l, r) (((l) <= (i)) && ((i) <= (r)))
+
+// iterator
+#define ALL(c) (c).begin(), (c).end()
+#define SORT(c) sort(ALL(c))
+#define REVERSE(c) reverse(ALL(c))
+#define HAS(c, v) ((c).find(v) != (c).end())
+#define WHILEIT(it, c) auto it = (c).begin(); while (it != (c).end())
+#define SUM(c) (reduce(ALL(c)))
+#define MAX(c) (max_element(ALL(c)))
+#define MIN(c) (min_element(ALL(c)))
+#define TOSET(s, c) set s(ALL(c))
+#define TOUSET(s, c) uset s(ALL(c))
+
+// loop
+#define FOR(i, l, r) for (int (i) = (l); (i) < (r); ++(i))
+#define FORE(i, l, r) for (int (i) = (l); (i) <= (r); ++(i))
+#define FFOR(i, l, r) for (int (i) = (l); (i) > (r); --(i))
+#define FFORE(i, l, r) for (int (i) = (l); (i) >= (r); --(i))
+#define FOREACH(v, c) for (auto &(v) : (c))
+#define REPEAT(i, n) FOR(i, 0, n)
+#define REPEATN(i, n) FORE(i, 1, n)
+
+#endif
+
+#ifndef __pretty_print__
+#define __pretty_print__
+
+#define IOS ios_base::sync_with_stdio(0) // to synchronize the input of cin and scanf
+#define BREAK cout << "===========" << endl
+#define VALUE(x) cout << "The value of \"" << #x << "\" is: " << x << endl
+
+template<typename K, typename V>
+ostream &operator<<(ostream &out, const pair<K, V> &pair)
+{
+    out << "<" << pair.first << ", " << pair.second << ">";
+    return out;
+}
+
+template<typename T>
+static inline ostream &__print_iterable(ostream &out, const T &container) 
+{
+    auto it = container.begin();
+    out << "{ " << *it++;
+    while (it != container.end()) {
+        out << ", " << *it++;
+    }
+    out << " }";
+    return out;
+}
+
+template<typename T>
+ostream &operator<<(ostream &out, const vector<T> &vec) {
+    return __print_iterable(out, vec);
+}
+
+template<typename T, size_t Size>
+ostream &operator<<(ostream &out, const array<T, Size> &arr) {
+    return __print_iterable(out, arr);
+}
+
+template<typename T>
+ostream &operator<<(ostream &out, const set<T> &set) {
+    return __print_iterable(out, set);
+}
+
+template<typename T>
+ostream &operator<<(ostream &out, const stack<T> &s) {
+    out << "[";
+    
+    if (!s.empty()) {
+        stack<T> copy = s;
+        out << copy.top();
+        copy.pop();
+
+        while (!copy.empty()) {
+            out << ", ";
+            auto top = copy.top();
+            out << top;
+            copy.pop();
+        }
+    }
+
+    out << "]";
+    return out;
+}
+
+template<typename T>
+ostream &operator<<(ostream &out, const queue<T> &q) {
+    out << "[";
+    
+    if (!q.empty()) {
+        queue<T> copy = q;
+        out << copy.front();
+        copy.pop();
+
+        while (!copy.empty()) {
+            out << ", ";
+            auto front = copy.front();
+            out << front;
+            copy.pop();
+        }
+    }
+
+    out << "]";
+    return out;
+}
+
+template<typename T>
+ostream &operator<<(ostream &out, const priority_queue<T> &pq) {
+    return __print_iterable(out, pq); // BUG
+}
+
+template<typename T>
+ostream &operator<<(ostream &out, const deque<T> &deq) {
+    return __print_iterable(out, deq); // BUG
+}
+
+template<typename K, typename V>
+ostream &operator<<(ostream &out, const map<K, V> &map) {
+    return __print_iterable(out, map);
+}
+
+template<typename K, typename V>
+ostream &operator<<(ostream &out, const unordered_map<K, V> &map) {
+    return __print_iterable(out, map);
+}
+#endif
+
+#ifndef __gadgets__
+#define __gadgets__
+
+template<typename T>
+struct CompareFn {
+    virtual bool operator() (const T &a, const T &b); // returns false if a is first and b is second.
+};
+
+#endif
+
+#endif
+
+class Solution {
+public:
+    int maxVowels(string s, int k) {
+        const int len = s.length();
+        
+        int currVowels = 0;
+        REPEAT(i, k) {
+            if (isVowel(s[i])) {
+                currVowels++;
+            }
+        }
+        
+        int maxVowels = currVowels;
+
+        FOR(i, k, len) {
+            char prevChar = s[i - k];
+            char currChar = s[i];
+            
+            if (isVowel(prevChar)) {
+                currVowels--;
+            }
+
+            if (isVowel(currChar)) {
+                currVowels++;
+            }
+
+            maxVowels = max(maxVowels, currVowels);
+        }
+
+        return maxVowels;
+    }
+
+    bool isVowel(char c) {
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            return true;
+        }
+        return false;
+    }
+};
+// @lc code=end
